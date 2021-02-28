328583d651061c76f41a77e2bedcf3791ca7eeaa
slavabdev <slav.balykov@gmail.com>
2021-02-28 19:39:19 +0000

    Deployment to Heroku

--- run.py
+++ run.py
@@ -1,10 +1,13 @@
 import os
-from flask import Flask, render_template
+import json
+from flask import Flask, render_template, request, flash
+if os.path.exists('env.py'):
+    import env
 
 
 # Creating an instance of the class and store it in variable app
 app = Flask(__name__)
-
+app.secret_key = os.environ.get("SECRET_KEY")
 
 # Route decorator will tell Flask what URL following function will trigger
 @app.route('/')
@@ -14,11 +17,27 @@ def index():
 
 @app.route('/about')
 def about():
-    return render_template("about.html", page_title='About')
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title='About', company=data)
 
 
-@app.route('/contact')
+@app.route('/about/<member_name>')
+def about_memebr(member_name):
+    member = {}
+    with open('data/company.json', 'r') as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj['url'] == member_name:
+                member = obj
+    return render_template('member.html', member=member)
+
+
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
+    if request.method == 'POST':
+        flash('Thanks {}, we have received your message!'.format(request.form.get("name")))
     return render_template("contact.html", page_title='Contacts')
 
 
